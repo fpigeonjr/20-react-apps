8cc0cb80b0c45ea305d152c2d5c27e40656f9291
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-05-15 14:08:19 -0500

    - starts pomodoro timer - stops at zero

--- pomodoro/start/src/App.js
+++ pomodoro/start/src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+// above default
+function padTime(time) {
+  return time.toString().padStart(2, '0')
+}
 
 export default function App() {
+  const [timeLeft, setTimeLeft] = useState(10)
+  const [title, setTitle] = useState('Let the countdown begin!!!')
+  const minutes = padTime(Math.floor(timeLeft / 60))
+  const seconds = padTime(timeLeft - minutes * 60)
+
+  function startTimer() {
+    setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1
+
+        // TODO: reset timer
+        return 0
+      })
+    }, 1000)
+  }
+
   return (
     <div className="app">
-      <h2>Pomodoro!</h2>
+      <h2>{title}</h2>
 
       <div className="timer">
-        <span>00</span>
+        <span>{minutes}</span>
         <span>:</span>
-        <span>00</span>
+        <span>{seconds}</span>
       </div>
 
       <div className="buttons">
-        <button>Start</button>
+        <button onClick={startTimer}>Start</button>
         <button>Stop</button>
         <button>Reset</button>
       </div>
     </div>
-  );
+  )
 }
